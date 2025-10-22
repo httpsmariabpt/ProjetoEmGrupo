741443e103f303a9f4ff42cc2c3b1dc1fc9e17e3
httpsmariabpt <maria.baptista@aluno.senai.br>
2025-10-22 13:46:34 -0300

    Estrutura Base Do Projeto

--- projeto/Data/ApplicationDbContext.cs
+++ projeto/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using projeto.Models;
 
 namespace projeto.Data
 {
@@ -9,5 +11,34 @@ namespace projeto.Data
             : base(options)
         {
         }
+
+        public DbSet<Animal> Animais { get; set; }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Consulta> Consultas { get; set; }
+
+        public DbSet<Veterinario> Veterinarios { get; set; }
+
+        public DbSet<Especialidade> Especialidades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //Aqui, nós dizemos como irá se chamar a tabela 
+            //no banco de dados
+            
+            base.OnModelCreating(builder);
+            // Exemplo de configuração sem cascata
+            
+            builder.Entity<Animal>().ToTable("Animais");
+            builder.Entity<Cliente>().ToTable("Clientes");
+            builder.Entity<Consulta>().ToTable("Consultas");
+            builder.Entity<Veterinario>().ToTable("Veterinarios");
+            builder.Entity<Especialidade>().ToTable("Especialidades");
+
+            //Depois que você cria, vocE^pode adicionar ao banco de dados
+            //Utilizando as migrations do Entity Framework
+        }
+
     }
 }
---
+++ projeto/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace projeto.Models
+{
+    public class Cliente
+    {
+        public Guid ClienteId { get; set; }
+        public string Nome { get; set; }
+        public Guid AnimalId { get; set; }
+        public Animal Animal { get; set; }
+        public string CPF { get; set; }
+        public string DataNasc { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+        public string Endereco { get; set; }
+    }
+}
